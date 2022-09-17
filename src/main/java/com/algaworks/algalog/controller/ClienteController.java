a243c5fa9658acf1c164f346f3f139e30222bed0
Matheus Rian <thenumber36@outlook.com>
2022-09-17 14:04:31 -0300

    Jakarta persistence

--- src/main/java/com/algaworks/algalog/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +13,13 @@ import com.algaworks.algalog.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setName("Joao");
-		cliente1.setEmail("joaodasneves@algaworks.com");
-		cliente1.setTelefone("34 99999-1111");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(1L);
-		cliente2.setName("Maria");
-		cliente2.setEmail("maria@algaworks.com");
-		cliente2.setTelefone("34 99999-1111");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return entityManager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
