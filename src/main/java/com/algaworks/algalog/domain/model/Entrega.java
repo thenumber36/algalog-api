76d76793175cd69c99d439986f3ba58f989dacd4
Matheus Rian <thenumber36@outlook.com>
2022-09-18 17:07:47 -0300

    Validation Groups

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -11,7 +11,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 
+import com.algaworks.algalog.domain.ValidationGroups;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -30,12 +35,18 @@ public class Entrega {
 	@EqualsAndHashCode.Include
 	private Long id;
 	
+	@Valid
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 		
+	@Valid
+	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
+	@NotNull
 	private BigDecimal taxa;
 	
 	@Enumerated(EnumType.STRING)
